088deba41b1a6dca5c8df10ee787f4f3dd1a6497
tzuk <tzuk9800@gmail.com>
2023-03-30 13:49:53 +0300

    getting ready for deploy

--- analyzeVideos.py
+++ analyzeVideos.py
@@ -19,6 +19,7 @@ conn = SMBConnection('LabRead',
 assert conn.connect('132.74.242.29', port=445)
 # list the files on each share
 files = conn.listPath(service_name_video_path, video_paths_smb, timeout=30)
+print(files)
 for file in files:
     if "." != file.filename and ".." != file.filename:
         conn = SMBConnection('LabRead',
@@ -52,7 +53,7 @@ for file in files:
             keypoints_only=False,
             destfolder=video_tmp_analysis_save_path
         )
-        os.remove(tmp_video_path[0])
+        # os.remove(tmp_video_path[0])
         for basedir, dirs, files in os.walk(video_tmp_analysis_save_path):
             conn = SMBConnection('LabRead',
                                  'KlavirReadLab20@#',
@@ -61,8 +62,9 @@ for file in files:
                                  use_ntlm_v2=True)
             assert conn.connect('132.74.242.29', port=445)
             for result_file in files:
-                with open(os.path.join(basedir, result_file),"rb") as f:
+                with open(os.path.join(basedir, result_file), "rb") as f:
                     print("send " + result_file + " to " + results_paths_smb)
-                    conn.storeFile(service_name_result_path, results_paths_smb+"/"+result_file, f, timeout=60 * 60, show_progress=True)
-                os.remove(os.path.join(basedir, result_file))
+                    conn.storeFile(service_name_result_path, results_paths_smb + "/" + result_file, f, timeout=60 * 60,
+                                   show_progress=True)
+                # os.remove(os.path.join(basedir, result_file))
 print("done!")
