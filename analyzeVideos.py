ec45c8f1e3a0b07e089e32fd35231ae58ef0242f
tzuk <tzuk9800@gmail.com>
2023-03-30 13:09:00 +0300

    getting ready for deploy

--- analyzeVideos.py
+++ analyzeVideos.py
@@ -49,17 +49,16 @@ for file in files:
             tmp_video_path,
             keypoints_only=False,
         )
-        os.remove(tmp_video_path[0])
-        basedir, dirs, files = os.walk(video_tmp_save_path)
-        conn = SMBConnection('LabRead',
-                             'KlavirReadLab20@#',
-                             '132.74.242.29',
-                             'WORKGROUP',
-                             use_ntlm_v2=True)
-        assert conn.connect('132.74.242.29', port=445)
-        for result_file in files:
-            with open(os.path.join(basedir, result_file)) as f:
-                print("send " + result_file + " to " + results_paths_smb)
-                conn.storeFile(service_name_result_path, results_paths_smb, f, timeout=60 * 60, show_progress=True)
-            os.remove(os.path.join(basedir, result_file))
+        for basedir, dirs, files in os.walk(video_tmp_save_path):
+            conn = SMBConnection('LabRead',
+                                 'KlavirReadLab20@#',
+                                 '132.74.242.29',
+                                 'WORKGROUP',
+                                 use_ntlm_v2=True)
+            assert conn.connect('132.74.242.29', port=445)
+            for result_file in files:
+                with open(os.path.join(basedir, result_file)) as f:
+                    print("send " + result_file + " to " + results_paths_smb)
+                    conn.storeFile(service_name_result_path, results_paths_smb, f, timeout=60 * 60, show_progress=True)
+                os.remove(os.path.join(basedir, result_file))
 print("done!")
