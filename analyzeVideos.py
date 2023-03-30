9d1f63edfd941836b51ed64b0be6bfb40665b2a2
tzuk <tzuk9800@gmail.com>
2023-03-30 13:13:27 +0300

    getting ready for deploy

--- analyzeVideos.py
+++ analyzeVideos.py
@@ -59,6 +59,6 @@ for file in files:
             for result_file in files:
                 with open(os.path.join(basedir, result_file)) as f:
                     print("send " + result_file + " to " + results_paths_smb)
-                    conn.storeFile(service_name_result_path, results_paths_smb, f, timeout=60 * 60, show_progress=True)
+                    conn.storeFile(service_name_result_path, results_paths_smb+"/"+result_file, f, timeout=60 * 60, show_progress=True)
                 os.remove(os.path.join(basedir, result_file))
 print("done!")
