d20facc06bb62c782f4403d933a5f790b41f0803
tzuk <tzuk9800@gmail.com>
2023-04-09 11:59:28 +0300

    getting ready for deploy

--- analyzeVideos.py
+++ analyzeVideos.py
@@ -51,18 +51,19 @@ for file in files:
         )
         # os.remove(tmp_video_path[0])
         for basedir, dirs, files in os.walk(tmp_video_path[0]):
-            conn = SMBConnection('LabRead',
-                                 'KlavirReadLab20@#',
-                                 '132.74.242.29',
-                                 'WORKGROUP',
-                                 use_ntlm_v2=True)
-            assert conn.connect('132.74.242.29', port=445)
-            for result_file in files:
-                if ".csv" in result_file or ".h5" in result_file or "labeled.mp4" in result_file:
-                    with open(os.path.join(basedir, result_file), "rb") as f:
-                        print("send " + result_file + " to " + results_paths_smb)
-                        conn.storeFile(service_name_result_path, results_paths_smb + "/" + result_file, f,
-                                       timeout=60 * 60,
-                                       show_progress=True)
+            if len(files) > 0:
+                conn = SMBConnection('LabRead',
+                                     'KlavirReadLab20@#',
+                                     '132.74.242.29',
+                                     'WORKGROUP',
+                                     use_ntlm_v2=True)
+                assert conn.connect('132.74.242.29', port=445)
+                for result_file in files:
+                    if ".csv" in result_file or ".h5" in result_file or "labeled.mp4" in result_file:
+                        with open(os.path.join(basedir, result_file), "rb") as f:
+                            print("send " + result_file + " to " + results_paths_smb)
+                            conn.storeFile(service_name_result_path, results_paths_smb + "/" + result_file, f,
+                                           timeout=60 * 60,
+                                           show_progress=True)
                 # os.remove(os.path.join(basedir, result_file))
 print("done!")
