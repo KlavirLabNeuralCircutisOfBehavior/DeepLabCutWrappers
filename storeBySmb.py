f059625d6118fa43fa0ee950a9c59a09e8bd16d9
tzuk <tzuk9800@gmail.com>
2023-05-16 13:30:12 +0300

    added arg parser to smb store

--- storeBySmb.py
+++ storeBySmb.py
@@ -23,14 +23,13 @@ def store(files_location, service_name, path_to_store, filters=[], delete=False)
                              use_ntlm_v2=True)
         assert conn.connect('132.74.242.29', port=445)
         for result_file in files:
-            with open(os.path.join(basedir, result_file), "rb") as f:
-                if filters is not None and len(filters) > 0:
-                    for fil in filters:
-                        if fil in result_file:
-                            storeFile(conn, os.path.join(basedir, result_file), path_to_store, service_name)
-                            break
-                else:
-                    storeFile(conn, os.path.join(basedir, result_file), path_to_store, service_name)
+            if filters is not None and len(filters) > 0:
+                for fil in filters:
+                    if fil in result_file:
+                        storeFile(conn, os.path.join(basedir, result_file), path_to_store, service_name)
+                        break
+            else:
+                storeFile(conn, os.path.join(basedir, result_file), path_to_store, service_name)
             if delete:
                 os.remove(os.path.join(basedir, result_file))
 
