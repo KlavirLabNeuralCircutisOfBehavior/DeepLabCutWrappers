50e151a0ee8633c0eb81c29d8384a412ed39c9c1
tzuk <tzuk9800@gmail.com>
2023-06-01 09:56:03 +0300

    adding a generic script to run sbatch file on haifa university cloud

--- runTrainAndAnalyzing.sh
+++ runTrainAndAnalyzing.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 mkdir "${HOME}/logs"
-#SBATCH -J dlc-benchmark
-#SBATCH -o "${HOME}/logs/%x.%j.out"
-#SBATCH -e "${HOME}/logs/%x.%j.err"
-#SBATCH --time=7-00:00:00
-#SBATCH --get-user-env
 cd ~
 config_file_name=$1
 config_path=$2
@@ -20,8 +15,6 @@ git clone https://github.com/KlavirLabNeuralCircutisOfBehavior/DeepLabCutWrapper
 cp /users/klavir/shared/pytorch:21.04-py3.sqsh "${containerName}"
 IFS=. read -r file_name zip <<< "${config_file_name}"
 mkdir "video_tmp_save_path${current_date}"
-srun --gpus=1 --container-image="${containerName}" --container-mounts="${HOME}:/${userName}" python3 "/${userName}/DeepLabCutWrappers/trainAndAnalyze.py" "${config_path}" "${service_name_config}" "${config_file_name}" "/${userName}" "/${userName}/${file_name}/config.yaml" "${video_paths_smb}" "/${userName}/video_tmp_save_path${current_date}" "${results_paths_smb}" "${service_name_video_path}" "${service_name_result_path}"
-rm "${containerName}"
-rm -rf "video_tmp_save_path${current_date}"
+sbatch /users/klavir/shared/runTrainAndAnalyzingSbatch.sh config_file_name config_path service_name_config video_paths_smb results_paths_smb service_name_video_path service_name_result_path file_name current_date containerName userName
 
 # how to run: sbatch ../shared/TrainAndAnalyze.sh groomingL2-roni-2023-05-15.zip /roni/ homes /shahaf/pain_and_grooming_batch2/Videos/post_surgery/glass/L2/ /tzuk/test/ PUBLIC deeplabcutfiles
---
+++ runTrainAndAnalyzingSbatch.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+config_file_name=$1
+config_path=$2
+service_name_config=$3
+video_paths_smb=$4
+results_paths_smb=$5
+service_name_video_path=$6
+service_name_result_path=$7
+file_name=$8
+current_date=$9
+containerName=${10}
+userName=${11}
+#SBATCH -J dlc-benchmark
+#SBATCH -o logs/%x.%j.out
+#SBATCH -e logs/%x.%j.err
+#SBATCH -D "/users/klavir/${userName}/"
+#SBATCH --time=7-00:00:00
+#SBATCH -G 1
+#SBATCH --get-user-env
+#SBATCH --nodes 1
+srun --gpus=1 --container-image="${containerName}" --container-mounts="${HOME}:/${userName}" python3 "/${userName}/DeepLabCutWrappers/trainAndAnalyze.py" "${config_path}" "${service_name_config}" "${config_file_name}" "/${userName}" "/${userName}/${file_name}/config.yaml" "${video_paths_smb}" "/${userName}/video_tmp_save_path${current_date}" "${results_paths_smb}" "${service_name_video_path}" "${service_name_result_path}"
+rm "${containerName}"
+rm -rf "video_tmp_save_path${current_date}"
