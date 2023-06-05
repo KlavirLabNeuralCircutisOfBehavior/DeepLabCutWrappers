44cdf996c1e517569423bc7f9e15347fff7f0125
tzuk <tzuk9800@gmail.com>
2023-06-05 12:20:15 +0300

    adding a generic script to run sbatch file on haifa university cloud

--- runTrainAndAnalyzingSbatch.sh
+++ runTrainAndAnalyzingSbatch.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+
+#SBATCH -J dlc-benchmark
+#SBATCH -o logs/%x.%j.out
+#SBATCH -e logs/%x.%j.err
+#SBATCH -D "/users/klavir/${userName}/"
+#SBATCH --time=7-00:00:00
+#SBACTH -G 1
 config_file_name=$1
 config_path=$2
 service_name_config=$3
@@ -10,14 +17,6 @@ file_name=$8
 current_date=$9
 containerName=${10}
 userName=${11}
-#SBATCH -J dlc-benchmark
-#SBATCH -o logs/%x.%j.out
-#SBATCH -e logs/%x.%j.err
-#SBATCH -D "/users/klavir/${userName}/"
-#SBATCH --time=7-00:00:00
-#SBATCH -G 1
-#SBATCH --get-user-env
-#SBATCH --nodes 1
 srun --gpus=1 --container-image="${containerName}" --container-mounts="${HOME}:/${userName}" python3 "/${userName}/DeepLabCutWrappers/trainAndAnalyze.py" "${config_path}" "${service_name_config}" "${config_file_name}" "/${userName}" "/${userName}/${file_name}/config.yaml" "${video_paths_smb}" "/${userName}/video_tmp_save_path${current_date}" "${results_paths_smb}" "${service_name_video_path}" "${service_name_result_path}"
 rm "${containerName}"
 rm -rf "video_tmp_save_path${current_date}"
